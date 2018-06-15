1499b917ea1dbb9736d4e0edc5275a38b41f35fb
Michelle <2eunzizi@gmail.com>
2018-06-15 17:07:04 -0400

    updated

--- app/src/main/java/com/example/administrator/calculator2/MainActivity.kt
+++ app/src/main/java/com/example/administrator/calculator2/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.administrator.fourfunctioncalculator
+package com.example.administrator.calculator2
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -12,9 +12,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val button1 = findViewById<Button>(R.id.button1) as Button
-        val button2 = findViewById<Button>(R.id.button2) as Button
-        val button3 = findViewById<Button>(R.id.button3) as Button
+        val button2 = findViewById<Button>(R.id.button3) as Button
+        val button3 = findViewById<Button>(R.id.button4) as Button
         val button4 = findViewById<Button>(R.id.button4) as Button
+        val button5 = findViewById<Button>(R.id.button5) as Button
+        val button6 = findViewById<Button>(R.id.button6) as Button
+
+
+
 
         button1.setOnClickListener {
             val result: Int = editText2.text.toString().toInt() + editText1.text.toString().toInt()
@@ -36,5 +41,17 @@ class MainActivity : AppCompatActivity() {
             textView3.text = result.toString()
         }
 
+        button5.setOnClickListener {
+            val result: Int = editText2.text.toString().toInt() % editText1.text.toString().toInt()
+            textView3.text = result.toString()
+        }
+
+        button6.setOnClickListener {
+            val result: Double = Math.sqrt (editText1.text.toString().toDouble())
+            textView3.setText(result.toString())
+        }
+
+
     }
 }
+
